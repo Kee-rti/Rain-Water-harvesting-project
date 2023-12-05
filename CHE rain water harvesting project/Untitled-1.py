3d923252d0222ec11d42046c3c2fd924fde222e3
kee-rti <kirtithakurrrr@gmail.com>
2023-12-05 15:28:51 +0530

    completed
    '

---
+++ CHE rain water harvesting project/Untitled-1.py
@@ -0,0 +1,18 @@
+class student:
+    def setName(self, name, Rollno, subject, marks):
+        self.name=name
+        self.Rollno=Rollno
+        self.subject=subject
+        self.marks=marks
+    def getName(self):
+        return self.name
+    
+s1=student()
+s2=student()
+
+
+name=input('name: ')
+Rollno=input('Rollno:')
+subject=input('subject:')
+marks=input('marks:')
+s1.set
